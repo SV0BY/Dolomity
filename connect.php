cae805cdc4ebc0394058e332556b7064e7bad5f0
Kuba <kuba.svoby.js@gmail.com>
2025-01-03 23:39:23 +0100

    krakenConnect

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "127.0.0.1";
+$username = "svobodajakub8";
+$password = "kr@KEN-021110";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=svobodajakub8", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
